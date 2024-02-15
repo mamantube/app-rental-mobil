22303cd304120003522d06082c99d0d0de0a699d
mamantube <firmanhiday.id007@gmail.com>
2024-02-15 11:50:13 +0700

    add seeding

--- src/models/roles.js
+++ src/models/roles.js
@@ -1,20 +1,23 @@
 import { Schema, model } from "mongoose";
 
-const rolesSchema = new Schema({
-    name: {
-        type: String,
-        required: true,    
-    },
-    deleted_at: {
-        type: Date,
-        required: null,
+const rolesSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,    
+        },
+        deleted_at: {
+            type: Date,
+            default: null,
+        },
+    },  
+    {
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+        }
     }
-},  {
-    timestamp: {
-        createdAt: "created_at",
-        updatedAt: "updated_at",
-    }
-});
+);
 
 const roleModel = model("Roles", rolesSchema)
 
--- src/utils/seed_db.js
+++ src/utils/seed_db.js
@@ -3,5 +3,11 @@ import roleModel from "../models/roles.js";
 export default async function () {
     const findDataRole = await roleModel.find({ deleted_at: null});
 
+    if (!findDataRole.length) {
+        let roleList = [{name: "admin"}, {name: "customer"}];
+
+        await roleModel.insertMany(roleList)
+    }
     console.log("ini", findDataRole);
+    return;
 }
