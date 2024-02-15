b2dd885cbb2d284c51eae1b3bf1a18e820990541
mamantube <firmanhiday.id007@gmail.com>
2024-02-15 15:25:11 +0700

    add hashing

--- src/utils/seed_db.js
+++ src/utils/seed_db.js
@@ -1,13 +1,34 @@
 import roleModel from "../models/roles.js";
+import userModel from "../models/users.js";
+import bcrypt from "bcrypt";
+import { EMAIL_ADMIN, PASSWORD_ADMIN } from "./unpublished.js";
 
 export default async function () {
     const findDataRole = await roleModel.find({ deleted_at: null});
+    const findDataUser = await userModel.find({ deleted_at: null});
 
     if (!findDataRole.length) {
         let roleList = [{name: "admin"}, {name: "customer"}];
 
         await roleModel.insertMany(roleList)
     }
-    console.log("ini", findDataRole);
+
+    if (!findDataUser.length) {
+        const findRoleAdmin = await roleModel.findOne({ name: "admin", deleted_at: null});
+
+        const passwordHashing = bcrypt.hashSync(PASSWORD_ADMIN, 10);
+
+        let userAdmin = {
+            first_name: "Super",
+            last_name: "Admin",
+            phone: "+62895534655956",
+            email: EMAIL_ADMIN,
+            password: passwordHashing,
+            role_id: findRoleAdmin._doc._id,
+        };
+
+        console.log("INI", userAdmin)
+    }
+
     return;
 }
--- src/utils/unpublished.js
+++ src/utils/unpublished.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 
 const PORT = process.env.PORT;
-
 const URL_MONGODB = process.env.URL_MONGODB;
+const EMAIL_ADMIN = process.env.EMAIL_ADMIN;
+const PASSWORD_ADMIN = process.env.PASSWORD_ADMIN;
 
 export {
     PORT,
     URL_MONGODB,
+    EMAIL_ADMIN,
+    PASSWORD_ADMIN,
 }
