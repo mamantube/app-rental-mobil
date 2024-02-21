9e21bdde74371733017b7ecd41b0c4158c1eef66
mamantube <firmanhiday.id007@gmail.com>
2024-02-21 23:53:13 +0700

    add message not found

--- src/controllers/user/list.get.js
+++ src/controllers/user/list.get.js
@@ -78,6 +78,8 @@ export default async function (req, res) {
             }],
             ).sort({ _id: sort_by }).skip(skip).limit(per_page);
 
+        if(!data.length) return message(res, 404, "Data user tidak ditemukan");
+
         const countDocument = await userModel.aggregate([
             ...filter, { $match: { "role_detail.name": "customer",
         deleted_at: null, }}], { $match: { "role_detail.name": "customer",
