7c7355f7bf21af320e1ececad8d58c0d8130e5e5
mamantube <firmanhiday.id007@gmail.com>
2024-02-28 10:50:03 +0700

    add env for cloudinary

--- src/utils/unpublished.js
+++ src/utils/unpublished.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 
-const PORT = process.env.PORT;
-const URL_MONGODB = process.env.URL_MONGODB;
-const EMAIL_ADMIN = process.env.EMAIL_ADMIN;
-const PASSWORD_ADMIN = process.env.PASSWORD_ADMIN;
+const PORT = process.env.PORT || 3000;
+const URL_MONGODB = process.env.URL_MONGODB || "" ;
+const EMAIL_ADMIN = process.env.EMAIL_ADMIN || "" ;
+const PASSWORD_ADMIN = process.env.PASSWORD_ADMIN || "" ;
 const SECRET_KEY = process.env.SECRET_KEY;
+const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 
 export {
     PORT,
