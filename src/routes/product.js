ce3bcf0553fa332eb317acb8bcc172e286ebde33
mamantube <firmanhiday.id007@gmail.com>
2024-02-29 16:16:38 +0700

    add all data product AAPI

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,9 +2,11 @@ import express from "express";
 import Create from "../controllers/product/create.post.js";
 import { authentication, admin} from "../middleware/auth.js"
 import UploadImg from "../middleware/uploadImg.js";
+import listAdm from "../controllers/product/listAdm.get.js";
 
 const productRoute = express.Router();
 
 productRoute.post("/product/new", authentication, admin, UploadImg, Create);
+productRoute.get("/product", authentication, admin, listAdm);
 
 export default productRoute;
