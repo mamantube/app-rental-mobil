8f8f2ccf659bee2f2ed34807909120c86bc0fdb5
mamantube <firmanhiday.id007@gmail.com>
2024-03-03 10:42:57 +0700

    fixing method

--- src/controllers/product/remove.delete.js
+++ src/controllers/product/remove.delete.js
@@ -18,10 +18,11 @@ export default async function (req, res) {
     try {
         const _id = req.params._id;
 
-        const findProductById = await productModel.findByIdAndUpdate( {_id}, {deleted_at: new Date()}, {new: true} );
+        const findProductById = await productModel.findOneAndUpdate({ _id, deleted_at: null }, {deleted_at: new Date()}, {new: true} );
 
         if (!findProductById)
             return message(res, 404, "Produk tidak ditemukan")
+
         message(res, 200, "Produk masuk ke dalam daftar perbaikan")
     } catch (error) {
         message(res, 500, error?.message || "Server internal error")
