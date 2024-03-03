b4a589adcdaa4156b81d75b47765e0ab35d3d2f3
mamantube <firmanhiday.id007@gmail.com>
2024-03-03 10:16:27 +0700

    add remove product API

---
+++ src/controllers/product/remove.delete.js
@@ -0,0 +1,29 @@
+import productModel from "../../models/products.js";
+import message from "../../utils/message.js";
+import { Types } from "mongoose"
+
+/**
+ * 
+ * @typedef {import("express").Request} ExpressRequest 
+ * @typedef {import("express").Response} ExpressResponse
+ */
+
+/**
+ * 
+ * @param {ExpressRequest} req 
+ * @param {ExpressResponse} res 
+ */
+
+export default async function (req, res) {
+    try {
+        const _id = req.params._id;
+
+        const findProductById = await productModel.findByIdAndUpdate( {_id}, {deleted_at: new Date()}, {new: true} );
+
+        if (!findProductById)
+            return message(res, 404, "Produk tidak ditemukan")
+        message(res, 200, "Produk masuk ke dalam daftar perbaikan")
+    } catch (error) {
+        message(res, 500, error?.message || "Server internal error")
+    }
+}
--- src/routes/product.js
+++ src/routes/product.js
@@ -4,11 +4,12 @@ import { authentication, admin} from "../middleware/auth.js"
 import UploadImg from "../middleware/uploadImg.js";
 import listAdm from "../controllers/product/listAdm.get.js";
 import Detail from "../controllers/product/detail.get.js";
-
+import Remove from "../controllers/product/remove.delete.js";
 const productRoute = express.Router();
 
 productRoute.post("/product/new", authentication, admin, UploadImg, Create);
 productRoute.get("/product", authentication, admin, listAdm);
-productRoute.get("/product/:_id", authentication, Detail);
+productRoute.get("/product/:_id", authentication, admin, Detail);
+productRoute.delete("/product/remove/:_id", authentication, admin, Remove);
 
 export default productRoute;
