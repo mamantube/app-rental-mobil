f3b6ef6f111487e0b27b1c682c272090f24db565
mamantube <firmanhiday.id007@gmail.com>
2024-03-14 23:05:12 +0700

    add product detail API on customer side

---
+++ src/controllers/product/customer/detailCust.get.js
@@ -0,0 +1,47 @@
+import productModel from "../../../models/products.js";
+import message from "../../../utils/message.js";
+import { Types } from "mongoose"
+
+/**
+ * 
+ * @typedef {import("express").Request} ExpressRequest 
+ * @typedef {import("express").Response} ExpressResponse
+ */
+
+/**
+ * 
+ * @param {ExpressRequest} req 
+ * @param {ExpressResponse} res 
+ */
+
+export default async function (req, res) {
+    try {
+        const _id = req.params._id;
+
+        const detail = await productModel.aggregate([
+            {
+                $match: {
+                    _id: new Types.ObjectId(_id)
+                }
+            },
+            {
+                $lookup: {
+                    from: "storages",
+                    foreignField: "_id",
+                    localField: "storage_id",
+                    as: "storage_detail",
+                },
+            },
+            {
+                $unwind: "$storage_detail",
+            }
+        ]);
+
+        if (!detail.length)
+            return message(res, 404, "Produk tidak ditemukan")
+
+        message(res, 200, "Detail pproduk", detail[0])
+    } catch (error) {
+        message(res, 500, error?.message || "Server internal error")
+    }
+}
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Create from "../controllers/product/create.post.js";
-import { authentication, admin} from "../middleware/auth.js"
+import { authentication, admin, customer} from "../middleware/auth.js"
 import UploadImg from "../middleware/uploadImg.js";
 import listAdm from "../controllers/product/listAdm.get.js";
 import Detail from "../controllers/product/detail.get.js";
@@ -8,6 +8,7 @@ import Remove from "../controllers/product/remove.delete.js";
 import Restore from "../controllers/product/restore.patch.js";
 import Update from "../controllers/product/update.put.js";
 import listCust from "../controllers/product/customer/listCust.get.js";
+import detailCust from "../controllers/product/customer/detailCust.get.js";
 
 const productRoute = express.Router();
 
@@ -18,5 +19,6 @@ productRoute.delete("/product/remove/:_id", authentication, admin, Remove);
 productRoute.patch("/product/restore/:_id", authentication, admin, Restore);
 productRoute.put("/product/:_id", authentication, admin, UploadImg, Update);
 productRoute.get("/customer/product", listCust);
+productRoute.get("/customer/product/:_id", authentication, customer, detailCust);
 
 export default productRoute;
