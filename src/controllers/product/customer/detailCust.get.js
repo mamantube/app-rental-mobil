6ad2c3f8f3a6a6d1e6781ab17a274c09ab4297cc
mamantube <firmanhiday.id007@gmail.com>
2024-03-22 11:30:32 +0700

    add deleted null on detail API filter

--- src/controllers/product/customer/detailCust.get.js
+++ src/controllers/product/customer/detailCust.get.js
@@ -1,16 +1,7 @@
 import productModel from "../../../models/products.js";
 import message from "../../../utils/message.js";
 import { Types } from "mongoose";
-import transactionModel from "../../../models/transaction.js";
-import validation from "../../../utils/validation.js";
-import { z } from "zod";
 
-const schemaValidation = z.object({
-    start_date: z.coerce.date(),
-    end_date: z.coerce.date(),
-}).refine((data) => {
-    return !data.end_date || data.start_date <= data.end_date
-}, "Start_date tidak boleh lebih dari end_date")
 
 /**
  * 
@@ -26,34 +17,12 @@ const schemaValidation = z.object({
 
 export default async function (req, res) {
     try {
-        const start_date = req.query.start_date;
-        const end_date = req.query.end_date;
-        
-        const checkValidation = validation(schemaValidation, req.query)
-        
-        if(!checkValidation.success)
-            return message(res, 422, "Validasi error", {
-                errors: checkValidation.errors,
-            });
-
-        const findTransaction = await transactionModel.find({
-            $or: [
-                {
-                    "rental_duration.start_date": { $lte: new Date(end_date) },
-                    "rental_duration.end_date": { $gte: new Date(start_date) },
-                },
-            ],
-            status: { $nin: ["failure", "refund"] },
-            deleted_at: null,
-        });
-
-        let product_ids = findTransaction.map((transaction) => transaction._doc.product_ids).flat();
-
-        // const _id = req.params._id;
+        const _id = req.params._id;
         const detail = await productModel.aggregate([
             {
                 $match: {
-                    _id: { $nin: product_ids},
+                    _id: new Types.ObjectId(_id),
+                    deleted_at: null,
                 },
             },
             {
@@ -72,7 +41,7 @@ export default async function (req, res) {
         if (!detail.length)
             return message(res, 404, "Produk tidak ditemukan")
 
-        message(res, 200, "Detail pproduk", detail[0], product_ids)
+        message(res, 200, "Detail pproduk", detail[0])
     } catch (error) {
         message(res, 500, error?.message || "Server internal error")
     }
