cc1b7f8856066f07b7419f81c0bdd8f39854d657
mamantube <firmanhiday.id007@gmail.com>
2024-03-26 14:42:26 +0700

    add detail transaction API

---
+++ src/controllers/transaction/detail.get.js
@@ -0,0 +1,61 @@
+import message from "../../utils/message.js";
+import transactionModel from "../../models/transaction.js";
+import { Types } from "mongoose";
+
+
+/**
+ * 
+ * @typedef {import("express").Request} ExpressRequest 
+ * @typedef {import("express").Response} ExpressResponse
+ */
+
+/**
+ * 
+ * @param {ExpressRequest} req 
+ * @param {ExpressResponse} res 
+ */
+
+export default async function (req, res) {
+    try {
+        const _id = req.params._id;
+        
+        const detail = await transactionModel.aggregate([
+            {
+                $match: {
+                    _id: new Types.ObjectId(_id),
+                    deleted_at: null,
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    foreignField: "_id",
+                    localField: "user_id",
+                    as: "user_detail"
+                },
+            },
+            {
+                $unwind: "$user_detail",
+            },
+            {
+                $lookup: {
+                    from: "products",
+                    foreignField: "_id",
+                    localField: "product_ids",
+                    as: "product_detail"
+                }
+            },
+            {
+                $project: {
+                    "user_detail.password": 0,
+                }
+            },
+        ]);
+
+        if (!detail.length)
+            return message(res, 404, "Transaksi tidak ditemukan")
+        message(res, 200, "Detail transaksi", detail)
+    } catch (error) {
+        message(res, 500, error?.message || "Server internal error")
+    }
+}
--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -2,10 +2,12 @@ import express from "express";
 import Checkout from "../controllers/transaction/checkout.post.js";
 import { authentication, customer} from "../middleware/auth.js"
 import List from "../controllers/transaction/list.get.js";
+import Detail from "../controllers/transaction/detail.get.js";
 
 const transactionRoute = express.Router();
 
 transactionRoute.post("/transaction/checkout", authentication, customer, Checkout)
 transactionRoute.get("/transaction", authentication, List);
+transactionRoute.get("/transaction/:_id", authentication, Detail);
 
 export default transactionRoute;
