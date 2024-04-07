8e471ae8bfa98a41fbd330b1e788c63ef8674db2
mamantube <firmanhiday.id007@gmail.com>
2024-04-07 13:02:33 +0700

    add update transaction routes

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -5,6 +5,7 @@ import List from "../controllers/transaction/list.get.js";
 import Detail from "../controllers/transaction/detail.get.js";
 import CheckStatus from "../controllers/transaction/checkstatus.post.js";
 import Refund from "../controllers/transaction/refund.patch.js";
+import Update from "../controllers/product/update.put.js";
 
 const transactionRoute = express.Router();
 
@@ -13,5 +14,6 @@ transactionRoute.get("/transaction", authentication, List);
 transactionRoute.get("/transaction/:_id", authentication, Detail);
 transactionRoute.post("/transaction/check-status/:order_id", authentication, CheckStatus);
 transactionRoute.patch("/transaction/refund/:_id", authentication, admin, Refund);
+transactionRoute.put("/transaction/:_id", authentication, customer, Update);
 
 export default transactionRoute;
