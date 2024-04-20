a86cb9e18cf358fd96bfe2058af77d1ba92f717d
mamantube <firmanhiday.id007@gmail.com>
2024-04-20 11:10:59 +0700

    update css

--- frontend/src/components/FormAuth.jsx
+++ frontend/src/components/FormAuth.jsx
@@ -7,10 +7,10 @@ export default function FormAuth (props) {
     return (
         <Card id="card__form__auth" className="shadow-sm">
             <Card.Body className="body__form">
-                <Card.Title className=" text-center text-h3 title__form">
+                <Card.Title className=" text-center text-dark text-h3 title__form">
                     {title}
                 </Card.Title>
-                <Card.Subtitle className="text-p4 subtitle__form">
+                <Card.Subtitle className="text-p4 text-dark subtitle__form">
                     {subTitle}
                 </Card.Subtitle>
 
