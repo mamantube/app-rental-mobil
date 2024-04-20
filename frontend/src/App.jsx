96740e1f24941f561d4f8176f3f7c13750e00852
mamantube <firmanhiday.id007@gmail.com>
2024-04-20 13:14:29 +0700

    add validation login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import layoutDashboard from "./layouts/Dashboard";
 import layoutLanding from "./layouts/Landing";
 import Home from "./pages/Home";
 import Admin from "./pages/admin/Index";
+import Adminlogin from "./pages/admin/Login";
 
 
 export default function App() {
@@ -12,9 +13,13 @@ export default function App() {
         <Route Component={layoutDashboard}>
           <Route index path="/admin" Component={Admin} />
         </Route>
+
         <Route Component={layoutLanding}>
           <Route index path="/" Component={Home} />
         </Route>
+
+        <Route path="/admin/login" Component={Adminlogin} />
+        
         <Route path="/*" element={<h1>Page Not Found :( </h1>} />
       </Routes>
     </BrowserRouter>
