da4539d7ae02a3d1e6c31834837042f22513cd43
mamantube <firmanhiday.id007@gmail.com>
2024-04-20 14:32:24 +0700

    tidy up folder and file

--- frontend/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, legacy_createStore as createStore } from "redux";
-import { reducerCounter} from "./reducers/counter.js";
-
-let store = combineReducers({
-    counter: reducerCounter,
-});
-
-export default createStore(store)
