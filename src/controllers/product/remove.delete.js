0f6a60c913143d3a69a494dd7bde8fa08c5b1c93
mamantube <firmanhiday.id007@gmail.com>
2024-05-24 15:22:58 +0700

    fix remove product API

--- src/controllers/product/remove.delete.js
+++ src/controllers/product/remove.delete.js
@@ -1,30 +1,33 @@
 import productModel from "../../models/products.js";
 import message from "../../utils/message.js";
-import { Types } from "mongoose"
+import { Types } from "mongoose";
 
 /**
- * 
- * @typedef {import("express").Request} ExpressRequest 
+ *
+ * @typedef {import("express").Request} ExpressRequest
  * @typedef {import("express").Response} ExpressResponse
  */
 
 /**
- * 
- * @param {ExpressRequest} req 
- * @param {ExpressResponse} res 
+ *
+ * @param {ExpressRequest} req
+ * @param {ExpressResponse} res
  */
 
 export default async function (req, res) {
-    try {
-        const _id = req.params._id;
+  try {
+    const _id = req.params._id;
 
-        const findProductById = await productModel.findOneAndUpdate({ _id, deleted_at: null }, {deleted_at: new Date()}, {new: true} );
+    const findProductById = await productModel.findOneAndUpdate(
+      { _id },
+      { deleted_at: new Date() },
+      { new: true }
+    );
 
-        if (!findProductById)
-            return message(res, 404, "Produk tidak ditemukan")
+    if (!findProductById) return message(res, 404, "Produk tidak ditemukan");
 
-        message(res, 200, "Produk masuk ke dalam daftar perbaikan")
-    } catch (error) {
-        message(res, 500, error?.message || "Server internal error")
-    }
+    message(res, 200, "Produk masuk ke dalam daftar perbaikan");
+  } catch (error) {
+    message(res, 500, error?.message || "Server internal error");
+  }
 }
