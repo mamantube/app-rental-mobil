d23bb6d9b754bd2211159d383be1be6c1555522e
mamantube <firmanhiday.id007@gmail.com>
2024-05-27 22:55:46 +0700

    try fix customer product API

--- src/controllers/product/customer/listCust.get.js
+++ src/controllers/product/customer/listCust.get.js
@@ -1,15 +1,15 @@
 import productModel from "../../../models/products.js";
 import message from "../../../utils/message.js";
-import transactionModel from "../../../models/transaction.js";
-import validation from "../../../utils/validation.js";
-import { z } from "zod";
+// import transactionModel from "../../../models/transaction.js";
+// import validation from "../../../utils/validation.js";
+// import { z } from "zod";
 
-const schemaValidation = z.object({
-    start_date: z.coerce.date(),
-    end_date: z.coerce.date(),
-}).refine((data) => {
-    return !data.end_date || data.start_date <= data.end_date
-}, "Start_date tidak boleh lebih dari end_date")
+// const schemaValidation = z.object({
+//     start_date: z.coerce.date(),
+//     end_date: z.coerce.date(),
+// }).refine((data) => {
+//     return !data.end_date || data.start_date <= data.end_date
+// }, "Start_date tidak boleh lebih dari end_date")
 
 /**
  *  
@@ -25,34 +25,34 @@ const schemaValidation = z.object({
 
 export default async function (req, res) {
     try {
-        const start_date = req.query.start_date;
-        const end_date = req.query.end_date;
+        // const start_date = req.query.start_date;
+        // const end_date = req.query.end_date;
 
-        const checkValidation = validation(schemaValidation, req.query)
+        // const checkValidation = validation(schemaValidation, req.query)
 
-        if(!checkValidation.success)
-            return message(res, 422, "Validasi error", {
-                errors: checkValidation.errors,
-            });
+        // if(!checkValidation.success)
+        //     return message(res, 422, "Validasi error", {
+        //         errors: checkValidation.errors,
+        //     });
 
-        const findTransaction = await transactionModel.find({
-            $or: [
-                {
-                    "rental_duration.start_date": { $lte: new Date(end_date) },
-                    "rental_duration.end_date": { $gte: new Date(start_date) },
-                },
-                {
-                    "rental_duration.start_date": { $gte: new Date(start_date), $lte: new Date(end_date)},
-                },
-                {
-                    "rental_duration.end_date": { $gte: new Date(start_date), $lte: new Date(end_date)},
-                },
-            ],
-            status: { $nin: ["failure", "refund"] },
-            deleted_at: null,
-        });
+        // const findTransaction = await transactionModel.find({
+        //     $or: [
+        //         {
+        //             "rental_duration.start_date": { $lte: new Date(end_date) },
+        //             "rental_duration.end_date": { $gte: new Date(start_date) },
+        //         },
+        //         {
+        //             "rental_duration.start_date": { $gte: new Date(start_date), $lte: new Date(end_date)},
+        //         },
+        //         {
+        //             "rental_duration.end_date": { $gte: new Date(start_date), $lte: new Date(end_date)},
+        //         },
+        //     ],
+        //     status: { $nin: ["failure", "refund"] },
+        //     deleted_at: null,
+        // });
 
-        let product_ids = findTransaction.map((transaction) => transaction._doc.product_ids).flat();
+        // let product_ids = findTransaction.map((transaction) => transaction._doc.product_ids).flat();
 
         const q = req.query.q || "";
         // const sort_by = req.query.sort_by ? req.query.sort_by : "desc";
@@ -62,24 +62,40 @@ export default async function (req, res) {
 
 
         const filter = [
+            // {
+            //     $match: {
+            //         name: { $regex: q, $options: "i"},
+            //         _id: { $nin: product_ids},
+            //         deleted_at: null,
+            //     },
+            // },
+            // {
+            //     $lookup: {
+            //         from: "storages",
+            //         foreignField: "_id",
+            //         localField: "storage_id",
+            //         as : "storage_detail",
+            //     },
+            // },
+            // {
+            //     $unwind: "$storage_detail"
+            // },
             {
                 $match: {
-                    name: { $regex: q, $options: "i"},
-                    _id: { $nin: product_ids},
-                    deleted_at: null,
-                },
+                    name: { $regex: q, $options: "i" }
+                }
             },
             {
                 $lookup: {
                     from: "storages",
                     foreignField: "_id",
                     localField: "storage_id",
-                    as : "storage_detail",
+                    as: "storage_detail",
                 },
             },
             {
-                $unwind: "$storage_detail"
-            },
+                $unwind: "$storage_detail",
+            }
         ];
 
         const data = await productModel.aggregate(filter).sort({ _id: "desc" }).skip(skip).limit(per_page);
